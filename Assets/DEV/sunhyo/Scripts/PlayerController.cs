52863c185ac47e5ff6f1ee190e241b4c528129b6
second-laptop <daysandmoons1@gmail.com>
2022-03-06 02:30:04 +0900

    테스트 플레이어 이동

--- Assets/DEV/sunhyo/Scripts/PlayerController.cs
+++ Assets/DEV/sunhyo/Scripts/PlayerController.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rigidBody;
-    public CapsuleCollider capsuleCollider;
-    // Start is called before the first frame update
+    private Rigidbody rigidBody;
+    private CapsuleCollider capsuleCollider;
+    private Animator animator;
+    private Transform tr;
+
+    [SerializeField]private float moveSpeed = 5.0f;
+    [SerializeField] private float turnSpeed = 3.0f;
+    [SerializeField] private float jumpForce = 5.0f;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         capsuleCollider = GetComponent<CapsuleCollider>();
+        animator = GetComponent<Animator>();
+        tr = GetComponent<Transform>(); 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        float r = Input.GetAxisRaw("Mouse X");
+
+        if(h != 0 || v != 0) animator.SetBool("Running", true);
+        else animator.SetBool("Running", false);
+
+        tr.Translate(Vector3.right * h * moveSpeed * Time.deltaTime);
+        tr.Translate(Vector3.forward * v * moveSpeed * Time.deltaTime);
+        tr.Rotate(Vector3.up * turnSpeed * r);
+
+        if(Input.GetButton("Jump"))
+            rigidBody.AddForce(Vector3.up * jumpForce);
     }
 }
