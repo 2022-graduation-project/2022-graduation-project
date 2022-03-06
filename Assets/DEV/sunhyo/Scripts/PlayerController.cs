2549a7add811d3655b6b381a4bf1e688e346ad6e
second-laptop <daysandmoons1@gmail.com>
2022-03-07 00:53:39 +0900

    [UI] HP & MP, Skill & Item Quick Slot

--- Assets/DEV/sunhyo/Scripts/PlayerController.cs
+++ Assets/DEV/sunhyo/Scripts/PlayerController.cs
@@ -9,9 +9,11 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private Transform tr;
 
+    private RaycastHit hit;
+
     [SerializeField]private float moveSpeed = 5.0f;
     [SerializeField] private float turnSpeed = 3.0f;
-    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float jumpForce = 10.0f;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -22,6 +24,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        Debug.DrawRay(tr.position, Vector3.down * 0.1f, Color.yellow);
+
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         float r = Input.GetAxisRaw("Mouse X");
@@ -33,7 +37,7 @@ public class PlayerController : MonoBehaviour
         tr.Translate(Vector3.forward * v * moveSpeed * Time.deltaTime);
         tr.Rotate(Vector3.up * turnSpeed * r);
 
-        if(Input.GetButton("Jump"))
-            rigidBody.AddForce(Vector3.up * jumpForce);
+        if(Input.GetButton("Jump") && Physics.Raycast(tr.position, Vector3.down, out hit, 0.5f))
+            rigidBody.velocity = tr.up * jumpForce;
     }
 }
