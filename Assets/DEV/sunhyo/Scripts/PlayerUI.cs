3bc22f12c71118c031411b5d70ee3cc14bae5776
sunhyo <daysandmoons1@gmail.com>
2022-03-23 16:15:14 +0900

    퀘스트 패널

--- Assets/DEV/sunhyo/Scripts/PlayerUI.cs
+++ Assets/DEV/sunhyo/Scripts/PlayerUI.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class PlayerUI : MonoBehaviour
 {
     // public PlayerController player;
+    // public Canvas canvas;
     public Text nameTxt;
     public Text levelTxt;
     public Text classTxt;
     public Image hpBar;
     public Image mpBar;
+
+    // void Start()
+    // {
+    //     // canvas = GetComponent<Canvas>();
+    //     Canvas.ForceUpdateCanvases();
+    // }
 
     // 아이템 사용
     void UseItem()
