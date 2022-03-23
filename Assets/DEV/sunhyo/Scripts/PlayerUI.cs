e4e8c264059ebbdec09d857f400e6c7e749d0660
sunhyo <daysandmoons1@gmail.com>
2022-03-24 00:03:56 +0900

    feat : quest NPC

--- Assets/DEV/sunhyo/Scripts/PlayerUI.cs
+++ Assets/DEV/sunhyo/Scripts/PlayerUI.cs
@@ -5,30 +5,9 @@ using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
-    // public PlayerController player;
-    // public Canvas canvas;
     public Text nameTxt;
     public Text levelTxt;
     public Text classTxt;
     public Image hpBar;
     public Image mpBar;
-
-    // void Start()
-    // {
-    //     // canvas = GetComponent<Canvas>();
-    //     Canvas.ForceUpdateCanvases();
-    // }
-
-    // 아이템 사용
-    void UseItem()
-    {
-
-    }
-    // 스킬 사용
-    void UseSkill()
-    {
-
-    }
-
-    void Fit(RectTransform Rect) => LayoutRebuilder.ForceRebuildLayoutImmediate(Rect);
 }
