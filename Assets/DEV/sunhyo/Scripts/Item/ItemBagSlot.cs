276ba6abd7607eb71cf920cca82df11cc7b67a35
sunhyo <daysandmoons1@gmail.com>
2022-05-18 17:42:48 +0900

    add : item bag prefab

---
+++ Assets/DEV/sunhyo/Scripts/Item/ItemBagSlot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.DEV.sunhyo.Scripts.Item
+{
+    public class ItemBagSlot : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
