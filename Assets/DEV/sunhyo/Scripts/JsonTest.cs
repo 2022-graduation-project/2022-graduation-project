6e7206a6655d4bb76bd77d5cacf7412f5d4aec27
sunhyo <daysandmoons1@gmail.com>
2022-05-26 16:50:35 +0900

    player UI prefab

---
+++ Assets/DEV/sunhyo/Scripts/JsonTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class JsonTest : MonoBehaviour
+{
+    void Start()
+    {
+        PlayerData playerData = PlayerManager.instance.playerData;
+        
+        // DataManager.instance.JsonToObject<PlayerData>(playerData);
+    }
+}
