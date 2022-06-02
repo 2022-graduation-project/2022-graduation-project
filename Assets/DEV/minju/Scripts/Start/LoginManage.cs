3fbb4718a5343b070711055b491aefd22c421111
LAPTOP-QTJ4JOR7\SM-PC <ehdnf99@sookmyung.ac.kr>
2022-06-02 17:02:05 +0900

    UI Asset download

--- Assets/DEV/minju/Scripts/Start/LoginManage.cs
+++ Assets/DEV/minju/Scripts/Start/LoginManage.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class LoginManage : MonoBehaviour
 {
+    public GameObject LoginWindow;
+
     public void ClickLoginButton()
     {
-
+        LoginWindow.SetActive(true);
     }
 
     // Start is called before the first frame update
