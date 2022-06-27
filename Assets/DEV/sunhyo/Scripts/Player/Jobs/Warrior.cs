a0abb50f0f5269ae4a220e51a26845cc4bec57d7
LAPTOP-QTJ4JOR7\SM-PC <ehdnf99@sookmyung.ac.kr>
2022-06-27 13:09:10 +0900

    Warrior 시작

--- Assets/DEV/sunhyo/Scripts/Player/Jobs/Warrior.cs
+++ Assets/DEV/sunhyo/Scripts/Player/Jobs/Warrior.cs
@@ -1,8 +1,31 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class Warrior : PlayerController
 {
+    public List<GameObject> monsterList;
+    RaycastHit hit;
+    float MaxDistance = 100f;
+    Vector3 offset;
+    public void Start()
+    {
+        offset = new Vector3(0f, 0.7f, 0.5f);
+    }
+    private void Update()
+    {
+
+        Debug.DrawRay(transform.position + offset, transform.forward * MaxDistance, Color.blue);
+        if (Physics.Raycast(transform.position + offset, transform.forward, out hit, MaxDistance))
+        {
+            GameObject temp = hit.collider.gameObject;
+            if(temp.tag == "Monster" && monsterList.Find(x => x == temp) == null)
+            {
+                monsterList.Add(temp);
+            }
+        };
+        
+    }
 
 }
