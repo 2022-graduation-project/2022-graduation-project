b269249d811158ecb7cb2440c2e7a14aa8aaf9c4
sunhyo <daysandmoons1@gmail.com>
2022-07-15 19:39:35 +0900

    ?

--- Assets/DEV/sunhyo/Scripts/Inventory/InventorySlot.cs
+++ Assets/DEV/sunhyo/Scripts/Inventory/InventorySlot.cs
@@ -1,39 +1,34 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-
-public class InventorySlot : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+public class InventorySlot : MonoBehaviour, IPointerClickHandler
 {
-<<<<<<< .merge_file_a09464
-
-    [SerializeField] private Image icon;
-    [SerializeField] private Image count_img;
-    [SerializeField] private Text count_txt;
-=======
     [SerializeField] Image icon;
     [SerializeField] Image count_img;
     [SerializeField] Text count_txt;
     GameObject iconObject;
->>>>>>> .merge_file_a14144
 
     public ItemData itemData = null;
 
     public void Set(ItemData _itemData)
     {
-<<<<<<< .merge_file_a09464
-        // itemData가 비어있거나 || 다른 아이템
-        if (itemData == null || itemData.item_name != _itemData.item_name)
-=======
         iconObject = transform.GetChild(0).gameObject;
 
         if (itemData != null)
->>>>>>> .merge_file_a14144
         {
-            // itemData = _itemData.DeepCopy();
-            itemData = _itemData; // InventorUI에서 넘어오니까 인스턴스 생성 없어도 가능할지도?
+            itemData.count += _itemData.count;
+            count_txt.text = itemData.count.ToString();
+            if(itemData.count > 1)
+                count_img.gameObject.SetActive(true);
+            else
+                count_img.gameObject.SetActive(false);
+        }
+            
+        else
+        {
+            itemData = _itemData.DeepCopy();
             icon.sprite = DataManager.instance.LoadSpriteFile(Application.dataPath + "/DEV/sunhyo/Assets/Items", _itemData.image_name);
             icon.sprite.name = _itemData.image_name;
 
@@ -48,15 +43,6 @@ public class InventorySlot : MonoBehaviour, IPointerClickHandler, IBeginDragHand
 
             SetColorA(1f);
         }
-        else // 동일 아이템
-        {
-            itemData.count += _itemData.count;
-            count_txt.text = itemData.count.ToString();
-            if (itemData.count > 1)
-                count_img.gameObject.SetActive(true);
-            else
-                count_img.gameObject.SetActive(false);
-        }
 
         if (itemData.count < 1)
             Reset();
@@ -64,13 +50,9 @@ public class InventorySlot : MonoBehaviour, IPointerClickHandler, IBeginDragHand
 
     public void Reset()
     {
-<<<<<<< .merge_file_a09464
-        //print("리셋");
-=======
         // 자식 오브젝트 Icon에서 아이템 스크립트 컴포넌트 삭제
         DeleteItemComponent();
 
->>>>>>> .merge_file_a14144
         SetColorA(0f);
         icon.sprite = null;
         count_txt.text = string.Empty;
@@ -78,17 +60,13 @@ public class InventorySlot : MonoBehaviour, IPointerClickHandler, IBeginDragHand
         itemData = null;
     }
 
-    void UseItem()
+    public void OnPointerClick(PointerEventData _eventData)
     {
-        if(InventoryUI.instance.UseItem(this.itemData))
+        if(itemData != null)
         {
-<<<<<<< .merge_file_a09464
-
-=======
             print(itemData.image_name);
             UseItem();
             //PlayerManager.instance.UseItem(itemData);
->>>>>>> .merge_file_a14144
         }
     }
 
@@ -99,121 +77,24 @@ public class InventorySlot : MonoBehaviour, IPointerClickHandler, IBeginDragHand
         icon.color = color;
     }
 
-    void UpdateItemCount()
+    void UseItem()
     {
-<<<<<<< .merge_file_a09464
-
-=======
         // 자식 오브젝트 Icon의 
         // 아이템 스크립트 Use() 호출하기
         CallEachItemUse();
         itemData.count--;
         Set();
->>>>>>> .merge_file_a14144
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+    void Set()
     {
-        //print("button : " + eventData.button);
-        //print("clickCount : " + eventData.clickCount);
-        //print("name : " + gameObject.name);
-
-        /* clickCount는 일부 기기에서 씹히는 경우도 있다고 함. 시간 계산으로 변경하는 게 좋을 듯 */
-        /* 좌클릭 및 드래그 : 아이템 이동 */
-        if (itemData != null && eventData.button == PointerEventData.InputButton.Left)
-        {
-            /* 좌클릭 * 2 : 아이템 사용 / 장비 장착 */
-            if(eventData.clickCount > 1)
-            {
-                UseItem();
-            }
-        }
-
-        /* 우클릭 : 아이템 사용 / 장비 장착 */
-        else if(itemData != null && eventData.button == PointerEventData.InputButton.Right)
-        {
-            UseItem();
-        }
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        if (itemData == null)
-            return;
-        
-        SetColorA(0.3f);
-
-        DragSlot.instance.Set(itemData);
-        DragSlot.instance.gameObject.SetActive(true);
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (itemData == null || DragSlot.instance.itemData == null)
-            return;
-
-        DragSlot.instance.transform.position = eventData.position;
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        if (itemData == null)
-            return;
-
-        // 아이템 이동 완료
-        if(DragSlot.instance.itemData == null)
-        {
+        count_txt.text = itemData.count.ToString();
+        if (itemData.count > 1)
+            count_img.gameObject.SetActive(true);
+        else if(itemData.count == 1)
+            count_img.gameObject.SetActive(false);
+        else
             Reset();
-            return;
-        }
-
-        // Swap
-        if (DragSlot.instance.itemData != itemData)
-        {
-            Set(DragSlot.instance.itemData);
-            DragSlot.instance.Reset();
-            DragSlot.instance.gameObject.SetActive(false);
-            return;
-        }
-
-        //아이템 버리기
-        if (eventData.pointerCurrentRaycast.gameObject == null)
-        {
-            Reset();
-            DragSlot.instance.Reset();
-            DragSlot.instance.gameObject.SetActive(false);
-            return;
-        }
-        //try
-        //{
-        //    print(eventData.pointerCurrentRaycast.gameObject);
-        //    print(eventData.pointerCurrentRaycast.gameObject.name);
-        //}
-        //catch (NullReferenceException e)
-        //{
-        //    print("Null exception");
-        //}
-        
-
-        SetColorA(1f);
-        DragSlot.instance.Reset();
-        DragSlot.instance.gameObject.SetActive(false);
-    }
-
-    public void OnDrop(PointerEventData eventData)
-    {
-        if (DragSlot.instance.itemData == null)
-            return;
-
-        if (itemData == null) // 빈 슬롯인 경우
-        {
-            Set(DragSlot.instance.itemData);
-            DragSlot.instance.Reset();
-        }
-        else if(DragSlot.instance.itemData != itemData) // Swap
-        {
-            Set(DragSlot.instance.itemData);
-        }
     }
 
     /*****************************************
