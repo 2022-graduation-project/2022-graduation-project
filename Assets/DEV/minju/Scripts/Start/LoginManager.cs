0dc5426a9b614acd772ce232879aa07de92c35ad
LAPTOP-QTJ4JOR7\SM-PC <ehdnf99@sookmyung.ac.kr>
2022-08-01 19:57:35 +0900

    [민주] 회원가입 초반 작업

--- Assets/DEV/minju/Scripts/Start/LoginManager.cs
+++ Assets/DEV/minju/Scripts/Start/LoginManager.cs
@@ -12,6 +12,10 @@ using System;
 
 public class LoginManager : MonoBehaviour
 {
+
+    /**************************
+            공통 전광판
+    ***************************/
     // Message 에 2초간 메시지 띄우기
     public TMP_Text MsgText;
     private IEnumerator sendMessageOnProfile(string message, Color color)
@@ -22,6 +26,9 @@ public class LoginManager : MonoBehaviour
         MsgText.text = "";
     }
 
+    /**************************
+            서버 선택창
+    ***************************/
     // 서버 선택 버튼 클릭 시
     // 로그인 여부 먼저 확인
     private bool isLoggedIn = false;
@@ -39,9 +46,13 @@ public class LoginManager : MonoBehaviour
         }
     }
 
+    /**************************
+              로그인창
+    ***************************/
     // 로그인 창에서 로그인 버튼 클릭 시
     // 해당 Login Data Json File에서 찾기
     private string loggedInUsername = "";
+    [Header("Login UI")]
     public TMP_InputField username;
     public TMP_InputField password;
     public TMP_Text errText;
@@ -96,7 +107,6 @@ public class LoginManager : MonoBehaviour
                 }
             }
         }
-        
     }
     
     [Header("Account / Login Window")]
@@ -125,4 +135,82 @@ public class LoginManager : MonoBehaviour
         print(loggedInUsername+" Login Success");
     }
 
+    /**************************
+            회원가입창
+    ***************************/
+    [Header("SignUp UI")]
+    public TMP_InputField c_username;
+    public TMP_InputField c_password;
+    public TMP_InputField conf_password;
+    public TMP_Text c_errText;
+    
+    public void Clicked_CreateNewAccount()
+    {
+        // Empty check
+        bool hasUS = false;
+        bool hasPW = false;
+        bool hasCPW = false;
+        string inputUN = c_username.text;
+        string inputPW = c_password.text;
+        string inputCPW = conf_password.text;
+        if(inputUN=="")
+        {
+            c_errText.text = "INVALID USERNAME";
+            return;
+        }
+        else    hasUS = true;
+
+        if(inputPW=="")
+        {
+            c_errText.text = "INVALID PASSWORD";
+            return;
+        }
+        else    hasPW = true;
+        
+        if(inputCPW=="")
+        {
+            c_errText.text = "INVALID PASSWORD";
+            return;
+        }
+        else    hasCPW = true;
+
+        // 정상적으로 아이디와 비밀번호가 입력됐을 때
+        if(hasUS && hasPW && hasCPW)
+        {
+            // 로그인 Json Data 찾기
+            Dictionary<string, LoginData> loginDict;
+            loginDict =
+                DataManager.instance.
+                LoadJsonFile<Dictionary<string, LoginData>>
+                (Application.dataPath + "/MAIN/Data", "login");
+            // 이미 존재하는 아이디일 때
+            if(loginDict.ContainsKey(inputUN))
+            {
+                errText.text = "USERNAME HAS ALREADY TAKEN";
+                return;
+            }
+            else
+            {
+                // 비밀번호가 너무 짧을 때
+                if (inputPW.Length < 5)
+                {
+                    errText.text = "PASSWORD IS TOO SHORT";
+                    return;
+                }
+
+                // 확인 비밀번호가 일치하지 않을 때
+                else if(inputPW != inputCPW)
+                {
+                    errText.text = "PASSWORD MUST MATCH";
+                    return;
+                }
+
+                // 모두 정상일 때
+                else
+                {
+                    
+                }
+            }
+        }
+    }
 }
